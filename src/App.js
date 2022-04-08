fbc04f0f0f356f7905736af8b11c87dea0c03723
itiselhe <elixwm@gmail.com>
2022-04-08 18:47:34 +0430

    add list of object to state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [list , setList] = useState([ ]);
+  const [inputValue , setInputValue] = useState(" ")
+
+  function addValue(e) {
+    setInputValue(e.target.value)
+  }
+
+  function add (e) {
+    e.preventDefault()
+    setList([...list , {title : inputValue , id: list.length} ])
+    setInputValue(" ")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>To Do List</h1>
+      <input onChange={addValue} value={inputValue}></input>
+      <button onClick={add}>add</button>
+      <button>show List</button>
+    </>
   );
 }
 
